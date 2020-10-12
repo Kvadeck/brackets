6138075f5a45a1c9c14fcd3e0c05d1de1c581523
kvadeck <kvadeck@yandex.ru>
2020-10-12 13:50:54 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+module.exports = function check(str, config) {
+
+  let strCopy = str;
+  let pairLen = Array.from(str).length / 2;
+
+  if (Array.from(str).length % 2 !== 0) return false;
+
+  // Config pair trans
+  const transPair = config.reduce((pr, cur) => {
+    pr.push(cur.join(''))
+    return pr;
+  }, []);
+
+  // Loop all over the str
+  for (let i = 0; i < pairLen; i++) {
+    for (let i = 0; i < transPair.length; i++) {
+      let el = transPair[i];
+      strCopy = strCopy.replace(el, '');
+    }
+  }
+
+  return (strCopy) ? false : true;
+
 }
+
+
